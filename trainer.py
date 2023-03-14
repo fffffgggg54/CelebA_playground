225c61ff7ef7f21f94e2ef8e3fa2f59be62cb161
Fredo Guan <fredo.guan@hotmail.com>
2023-03-14 02:37:08 -0700

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -667,14 +667,14 @@ if __name__ == '__main__':
                         labels.numpy(force=True),
                         outputs.sigmoid().numpy(force=True)
                     )
-                    with torch.no_grad():
+                    #with torch.no_grad():
                     #    targs = torch.where(preds > boundary.detach(), torch.tensor(1).to(preds), labels) # hard SPLC
                     #    targs = ((1-labels)*stepAtThreshold(labels, boundary.detach()) + labels).detach().clone() # soft SPLC
                     
                     #shiftedLogits = outputs + torch.special.logit(boundary.detach().clone(), eps=1e-12)
-                    #loss = criterion(outputs, labels)
+                    loss = criterion(outputs, labels)
                     
-                    loss = criterion(outputs, targs) if epoch > 0 else criterion(outputs, labels)
+                    #loss = criterion(outputs, targs) if epoch > 0 else criterion(outputs, labels)
                     #loss = criterion(shiftedLogits, targs) if epoch > 0 else criterion(outputs, labels)
                     #loss = criterion(outputs + torch.special.logit(boundary.detach(), eps=1e-12), labels)
                     #criterion.tau_per_class = boundary + 0.1
